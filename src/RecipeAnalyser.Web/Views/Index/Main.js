e30ade7408a0d5f8f8550b18bb808fa0dbab7325
Ric <ric.fra@hotmail.it>
2017-08-26 11:15:40 +0100

    Moves ingredients list to a service.

--- src/RecipeAnalyser.Web/Views/Index/Main.js
+++ src/RecipeAnalyser.Web/Views/Index/Main.js
@@ -1,6 +1,19 @@
 
 var app = angular.module('recipeApp', []);
 
+app.factory('ingredientsSvc', [function () {
+    return [
+        { key: "default", value: "Please select an ingredient..." },
+        { key: "whiteFlour", value: "White flour", carbs: { total: 71.2, sugars: 1.8 }, fats: { total: 2.2, saturated: 1.4 }, fibre: 2.9, proteins: 10.5, salt: 0 },
+        { key: "erythritol", value: "Erythritol", carbs: { total: 0, sugars: 0 }, fats: { total: 0, saturated: 0 }, fibre: 0, proteins: 0, salt: 0 },
+        { key: "egg", value: "Eggs", fats: { total: 9.6, saturated: 2.7 }, carbs: { total: 0.25, sugars: 0.25 }, proteins: 14.1, fibre: 0.25, salt: 0.38 },
+        { key: "milk", value: "Milk", fats: { total: 3.6, saturated: 2.3 }, carbs: { total: 4.6, sugars: 4.6 }, proteins: 3.4, fibre: 0.25, salt: 0.11 },
+        { key: "mascarpone", value: "Mascarpone", fats: { total: 42, saturated: 30.4 }, carbs: { total: 4.9, sugars: 4.6 }, proteins: 3.6, fibre: 0, salt: 0.13 },
+        { key: "activeYeast", value: "Active yeast", fats: { total: 3.8, saturated: 1.3 }, carbs: { total: 16.9, sugars: 0.25 }, proteins: 44.5, fibre: 19.2, salt: 0.35 },
+        { key: "bakingPowder", value: "Baking powder", fats: { total: 0, saturated: 0 }, carbs: { total: 28, sugars: 0 }, proteins: 0, fibre: 0.2, salt: 0 },
+        { key: "butter", value: "Butter", fats: { total: 2.2, saturated: 1.4 }, carbs: { total: 71.2, sugars: 1.8 }, proteins: 10.5, fibre: 2.9, salt: 0.1 },
+        { key: "oatsFlour", value: "Oatmeal flour", fats: { total: 8.4, saturated: 1.3 }, carbs: { total: 56.1, sugars: 1 }, proteins: 12.1, fibre: 9.1, salt: 0 }];
+}]);
 
 app.controller('RecipeCtrl', RecipeCtrl);
 app.controller('IngredientCtrl', IngredientCtrl);
@@ -19,7 +32,7 @@ app.directive('dirEnter', function () {
     };
 });
 
-function RecipeCtrl($scope) {
+function RecipeCtrl($scope, ingredientsSvc) {
     $scope.carbsTotal = 0;
     $scope.sugarsTotal = 0;
     $scope.fatsTotal = 0;
@@ -31,22 +44,23 @@ function RecipeCtrl($scope) {
         AddIngredient($scope);
     }
 
-    $scope.ingredients = [
-        { key: "default", value: "Please select an ingredient..." },
-        { key: "whiteFlour", value: "White flour", carbs: { total: 71.2, sugars: 1.8 }, fats: { total: 2.2, saturated: 1.4 }, fibre: 2.9, proteins: 10.5, salt: 0 },
-        { key: "erythritol", value: "Erythritol", carbs: { total: 0, sugars: 0 }, fats: { total: 0, saturated: 0 }, fibre: 0, proteins: 0, salt: 0 },
-        { key: "egg", value: "Eggs", fats: { total: 9.6, saturated: 2.7 }, carbs: { total: 0.25, sugars: 0.25 }, proteins: 14.1, fibre: 0.25, salt: 0.38 },
-        { key: "milk", value: "Milk", fats: { total: 3.6, saturated: 2.3 }, carbs: { total: 4.6, sugars: 4.6 }, proteins: 3.4, fibre: 0.25, salt: 0.11 },
-        { key: "mascarpone", value: "Mascarpone", fats: { total: 42, saturated: 30.4 }, carbs: { total: 4.9, sugars: 4.6 }, proteins: 3.6, fibre: 0, salt: 0.13 },
-        { key: "activeYeast", value: "Active yeast", fats: { total: 3.8, saturated: 1.3 }, carbs: { total: 16.9, sugars: 0.25 }, proteins: 44.5, fibre: 19.2, salt: 0.35 },
-        { key: "bakingPowder", value: "Baking powder", fats: { total: 0, saturated: 0 }, carbs: { total: 28, sugars: 0 }, proteins: 0, fibre: 0.2, salt: 0 },
-        { key: "butter", value: "Butter", fats: { total: 2.2, saturated: 1.4 }, carbs: { total: 71.2, sugars: 1.8 }, proteins: 10.5, fibre: 2.9, salt: 0.1 },
-        { key: "oatsFlour", value: "Oatmeal flour", fats: { total: 8.4, saturated: 1.3 }, carbs: { total: 56.1, sugars: 1 }, proteins: 12.1, fibre: 9.1, salt: 0 }];
+    $scope.ingredients = ingredientsSvc;
+    //$scope.ingredients = [
+    //    { key: "default", value: "Please select an ingredient..." },
+    //    { key: "whiteFlour", value: "White flour", carbs: { total: 71.2, sugars: 1.8 }, fats: { total: 2.2, saturated: 1.4 }, fibre: 2.9, proteins: 10.5, salt: 0 },
+    //    { key: "erythritol", value: "Erythritol", carbs: { total: 0, sugars: 0 }, fats: { total: 0, saturated: 0 }, fibre: 0, proteins: 0, salt: 0 },
+    //    { key: "egg", value: "Eggs", fats: { total: 9.6, saturated: 2.7 }, carbs: { total: 0.25, sugars: 0.25 }, proteins: 14.1, fibre: 0.25, salt: 0.38 },
+    //    { key: "milk", value: "Milk", fats: { total: 3.6, saturated: 2.3 }, carbs: { total: 4.6, sugars: 4.6 }, proteins: 3.4, fibre: 0.25, salt: 0.11 },
+    //    { key: "mascarpone", value: "Mascarpone", fats: { total: 42, saturated: 30.4 }, carbs: { total: 4.9, sugars: 4.6 }, proteins: 3.6, fibre: 0, salt: 0.13 },
+    //    { key: "activeYeast", value: "Active yeast", fats: { total: 3.8, saturated: 1.3 }, carbs: { total: 16.9, sugars: 0.25 }, proteins: 44.5, fibre: 19.2, salt: 0.35 },
+    //    { key: "bakingPowder", value: "Baking powder", fats: { total: 0, saturated: 0 }, carbs: { total: 28, sugars: 0 }, proteins: 0, fibre: 0.2, salt: 0 },
+    //    { key: "butter", value: "Butter", fats: { total: 2.2, saturated: 1.4 }, carbs: { total: 71.2, sugars: 1.8 }, proteins: 10.5, fibre: 2.9, salt: 0.1 },
+    //    { key: "oatsFlour", value: "Oatmeal flour", fats: { total: 8.4, saturated: 1.3 }, carbs: { total: 56.1, sugars: 1 }, proteins: 12.1, fibre: 9.1, salt: 0 }];
 
     $scope.ingredientSelect = { key: "default", value: "Please select an ingredient..." };
 }
 
-function IngredientCtrl($scope) {
+function IngredientCtrl($scope, ingredientsSvc) {
 
 }
 
