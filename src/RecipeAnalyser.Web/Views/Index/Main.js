b65ff27a890dada7df797f47791149730a0c06fd
Ric <ric.fra@hotmail.it>
2017-08-27 10:42:06 +0100

    Completes ingredient amounts calculation functionality.

--- src/RecipeAnalyser.Web/Views/Index/Main.js
+++ src/RecipeAnalyser.Web/Views/Index/Main.js
@@ -45,32 +45,48 @@ function RecipeCtrl($scope, ingredientsSvc) {
     }
 
     $scope.ingredients = ingredientsSvc;
-    //$scope.ingredients = [
-    //    { key: "default", value: "Please select an ingredient..." },
-    //    { key: "whiteFlour", value: "White flour", carbs: { total: 71.2, sugars: 1.8 }, fats: { total: 2.2, saturated: 1.4 }, fibre: 2.9, proteins: 10.5, salt: 0 },
-    //    { key: "erythritol", value: "Erythritol", carbs: { total: 0, sugars: 0 }, fats: { total: 0, saturated: 0 }, fibre: 0, proteins: 0, salt: 0 },
-    //    { key: "egg", value: "Eggs", fats: { total: 9.6, saturated: 2.7 }, carbs: { total: 0.25, sugars: 0.25 }, proteins: 14.1, fibre: 0.25, salt: 0.38 },
-    //    { key: "milk", value: "Milk", fats: { total: 3.6, saturated: 2.3 }, carbs: { total: 4.6, sugars: 4.6 }, proteins: 3.4, fibre: 0.25, salt: 0.11 },
-    //    { key: "mascarpone", value: "Mascarpone", fats: { total: 42, saturated: 30.4 }, carbs: { total: 4.9, sugars: 4.6 }, proteins: 3.6, fibre: 0, salt: 0.13 },
-    //    { key: "activeYeast", value: "Active yeast", fats: { total: 3.8, saturated: 1.3 }, carbs: { total: 16.9, sugars: 0.25 }, proteins: 44.5, fibre: 19.2, salt: 0.35 },
-    //    { key: "bakingPowder", value: "Baking powder", fats: { total: 0, saturated: 0 }, carbs: { total: 28, sugars: 0 }, proteins: 0, fibre: 0.2, salt: 0 },
-    //    { key: "butter", value: "Butter", fats: { total: 2.2, saturated: 1.4 }, carbs: { total: 71.2, sugars: 1.8 }, proteins: 10.5, fibre: 2.9, salt: 0.1 },
-    //    { key: "oatsFlour", value: "Oatmeal flour", fats: { total: 8.4, saturated: 1.3 }, carbs: { total: 56.1, sugars: 1 }, proteins: 12.1, fibre: 9.1, salt: 0 }];
 
     $scope.ingredientSelect = { key: "default", value: "Please select an ingredient..." };
 }
 
-function IngredientCtrl($scope, ingredientsSvc) {
+function IngredientCtrl($scope) {
 
+    $scope.calculateAmount = function (ingredient) {
+        var ingredients = $scope.ingredients;
+        CalculateAmounts(ingredients, ingredient);
+    };
+    $scope.quantity = 0;
 }
 
 
 //functions
 
-function CalculateAmount(scope) {
-    var elementKey = scope.key;
+function CalculateAmounts(ingredients, currentIngredient) {
+    var elementKey = currentIngredient.key;
 
-    var element = SingleOrDefaultElementByKey(scope.recipeIngredients, elementKey);
+    var currentIngredientDefault = SingleOrDefaultElementByKey(ingredients, elementKey);
+    var ingredientQuantity = currentIngredient.quantity;
+
+    currentIngredient.carbs.total = CalculateAmount(currentIngredientDefault, 'carbs.total', ingredientQuantity);
+    currentIngredient.carbs.sugars = CalculateAmount(currentIngredientDefault, 'carbs.sugar', ingredientQuantity);
+    currentIngredient.fats.total = CalculateAmount(currentIngredientDefault, 'fats.total', ingredientQuantity);
+    currentIngredient.fats.saturated = CalculateAmount(currentIngredientDefault, 'fats.saturated', ingredientQuantity);
+    currentIngredient.proteins = CalculateAmount(currentIngredientDefault, 'proteins', ingredientQuantity);
+    currentIngredient.fibre = CalculateAmount(currentIngredientDefault, 'fibre', ingredientQuantity);
+
+}
+
+function CalculateAmount(currentIngredientDefault, defaultValueName, quantity) {
+    var result;
+    if (quantity) {
+        var referenceValue = GetDescendantProp(currentIngredientDefault, defaultValueName);
+
+        result = referenceValue / 100 * quantity;
+    } else {
+        result = 0;
+    }
+
+    return result;
 }
 
 function SingleOrDefaultElementByKey(elements, key) {
@@ -81,8 +97,9 @@ function SingleOrDefaultElementByKey(elements, key) {
     if (filteredElements.length > 1) {
         throw "Duplicates keys are not allowed in the ingredientSelect array!";
     }
+    var result = filteredElements[0];
 
-    return filteredElements.first;
+    return result;
 }
 
 function AddIngredient(scope) {
@@ -94,9 +111,16 @@ function AddIngredient(scope) {
     var isElementPresent = filteredElement;
     var isElementNotDefault = scope.ingredientSelect.key === "default";
 
-
     if (!isElementPresent && !isElementNotDefault) {
-        scope.recipeIngredients.push(scope.ingredientSelect);
+        var selectedIngredient = scope.ingredientSelect;
+        selectedIngredient.carbs.total = 0;
+        selectedIngredient.carbs.sugars = 0;
+        selectedIngredient.fats.total = 0;
+        selectedIngredient.fats.saturated = 0;
+        selectedIngredient.proteins = 0;
+        selectedIngredient.fibre = 0;
+
+        scope.recipeIngredients.push(selectedIngredient);
         UpdateTotals(scope);
     }
 }
@@ -128,6 +152,13 @@ function UpdateTotals($scope) {
     $scope.fibreTotal = SumObjectsArrayProperty($scope.recipeIngredients, 'fibre').toFixed(2);
 
 }
+
+function GetDescendantProp(obj, propertyPath) {
+    var arr = propertyPath.split(".");
+    while (arr.length && (obj = obj[arr.shift()]));
+    return obj;
+}
+
 
 
 
